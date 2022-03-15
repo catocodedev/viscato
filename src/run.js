d8c9f495a8b06a4e16f48ea8faed97bf317a1401
EmeraldMike <EmeraldMike360@gmail.com>
2022-03-14 20:29:32 -0400

    now can do any file

--- src/run.js
+++ src/run.js
@@ -1,12 +1,13 @@
+const { argv } = require('process');
 const process = require('./processer');
 const fs = require('fs');
-main();
-async function main() {
-var neew = await process.run('examples/home.catv');
-console.log(neew);
+console.log(argv);
+main(argv);
+async function main(args) {
+var neew = await process.run(args[2]+'.catv');
 // write neww result to file
-fs.writeFile('examples/rendered/home.html', neew, function(err) {
+fs.writeFile('./rendered/'+args[2]+'.html', neew, function(err) {
     if (err) throw err;
-    console.log('Saved!');
+    console.log('Rendered!');
 });
 }
